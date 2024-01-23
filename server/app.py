32794bbbd0ba70ff2a426262569a0d371e85d2ec
Karuga Mbugua <karugambugua@icloud.com>
2024-01-23 14:47:00 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -4,5 +4,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing Views</h1>'
+
+@app.route('/print/<string: name>')
+def print_string(name):
+    return f'<h1>Your named is {name}</h1>'
+
+@app.route('count/<integer: number>')
+def count(number):
+    x = range(number)
+    for i in x:
+        return i
+    
+@app.route('/math/<integer: num1, string: operation, integer: num2>')
+def math(num1, operation, num2):
+    sum = f'The sum is {num1} {operation} {num2}'
+    return sum
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
